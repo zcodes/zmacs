b698fe6bf2a9b10625b1cb37d76ba58e9a2ed59c
zcodes <zcodes@163.com>
2015-10-02 02:30:55 +0800

    setup file coding system

--- base/zmacs-base-edit.el
+++ base/zmacs-base-edit.el
@@ -1,5 +1,12 @@
 ;; Enable ido-mode
 (ido-mode t)
 
+;; Coding System
+(prefer-coding-system 'utf-8-unix)
+(if (eq system-type 'windows-nt)
+    (progn
+      (setq w32-unicode-filenames t)
+      (setq file-name-coding-system 'gbk)))
+
 (provide 'zmacs-base-edit)
 ;; End of zmacs-base-edit.el
