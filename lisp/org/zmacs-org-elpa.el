d31482e4508c6d38982dc905e3d3a469ef2a4ad2
zcodes <zcodes@163.com>
2015-11-04 00:34:58 +0800

    fixed org-mode install checked for emacs 25.0

--- lisp/org/zmacs-org-elpa.el
+++ lisp/org/zmacs-org-elpa.el
@@ -2,13 +2,12 @@
 
 (if (fboundp 'package-desc-p)
   (let* ((org-package (cadr (assoc 'org package-archive-contents)))
-         (org-package-version (format "%s" (package-desc-version org-package)))
+         (org-package-version (format "%s" (zmacs/elpa-package-version 'org)))
          (org-package-installed-path
            (expand-file-name (concat "org-" org-package-version)
                              package-user-dir)))
     (setf (package-desc-dir org-package) org-package-installed-path)
     (zmacs/elpa-install org-package))
   (zmacs/elpa-install 'org (list (zmacs/elpa-package-version 'org))))
-
 
 (provide 'zmacs-org-elpa)
